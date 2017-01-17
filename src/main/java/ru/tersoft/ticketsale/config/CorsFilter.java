d73699898815ea2cd4cadad38141265c59c25b16
Ilya Vyanni <sproot96@gmail.com>
2017-01-17 21:07:40 +0300

    Change CORS configuration

---
+++ src/main/java/ru/tersoft/ticketsale/config/CorsFilter.java
@@ -0,0 +1,30 @@
+package ru.tersoft.ticketsale.config;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Project ticketsale.
+ * Created by ivyanni on 17.01.2017.
+ */
+@Component
+public class CorsFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        if ("OPTIONS".equals(request.getMethod())) {
+            response.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, OPTIONS, DELETE");
+            response.setHeader("Access-Control-Max-Age", "3600");
+            response.setHeader("Access-Control-Allow-Credentials", "true");
+            response.setHeader("Access-Control-Allow-Headers", "authorization, content-type, accept");
+        }
+        filterChain.doFilter(request, response);
+    }
+}
--- src/main/java/ru/tersoft/ticketsale/config/ResourceServerConfig.java
+++ src/main/java/ru/tersoft/ticketsale/config/ResourceServerConfig.java
@@ -1,18 +1,23 @@
 package ru.tersoft.ticketsale.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.web.access.channel.ChannelProcessingFilter;
 
 import static org.springframework.http.HttpMethod.*;
 
 @Configuration
 @EnableResourceServer
+@Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
 @ComponentScan({ "ru.tersoft.ticketsale" })
 public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
     private final TokenStore tokenStore;
@@ -31,12 +36,13 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
     @Override
     public void configure(HttpSecurity http) throws Exception {
         http
+                .addFilterBefore(new CorsFilter(), ChannelProcessingFilter.class)
+                .csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
                 .authorizeRequests()
                 .antMatchers(OPTIONS, "/**").permitAll()
                 .antMatchers(POST, "/api/accounts").permitAll()
                 .antMatchers(GET, "/**").permitAll()
-                .antMatchers(GET, "/api/accounts/check/**").permitAll()
-                .antMatchers(GET, "/api/attractions/**").permitAll()
                 .antMatchers(GET, "/apidocs/**").permitAll()
                 .antMatchers(GET, "/webjars/springfox-swagger-ui/**").permitAll()
                 .antMatchers(GET, "/v2/api-docs/**").permitAll()
