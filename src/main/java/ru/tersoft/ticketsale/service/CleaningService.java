635266003da9d5821aaa74270f7251499b53d890
Ilya Vyanni <sproot96@gmail.com>
2017-01-24 14:34:02 +0300

    Create scheduled task for disabling expired Ticket entries

--- src/main/java/ru/tersoft/ticketsale/service/CleaningService.java
+++ src/main/java/ru/tersoft/ticketsale/service/CleaningService.java
@@ -7,8 +7,12 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import ru.tersoft.ticketsale.entity.Attraction;
 import ru.tersoft.ticketsale.entity.Maintenance;
+import ru.tersoft.ticketsale.entity.Order;
+import ru.tersoft.ticketsale.entity.Ticket;
 import ru.tersoft.ticketsale.repository.AttractionRepository;
 import ru.tersoft.ticketsale.repository.MaintenanceRepository;
+import ru.tersoft.ticketsale.repository.OrderRepository;
+import ru.tersoft.ticketsale.repository.TicketRepository;
 
 import java.util.Calendar;
 import java.util.List;
@@ -23,11 +27,15 @@ import java.util.logging.Logger;
 public class CleaningService {
     private final MaintenanceRepository maintenanceRepository;
     private final AttractionRepository attractionRepository;
+    private final OrderRepository orderRepository;
+    private final TicketRepository ticketRepository;
 
     @Autowired
-    public CleaningService(MaintenanceRepository maintenanceRepository, AttractionRepository attractionRepository) {
+    public CleaningService(MaintenanceRepository maintenanceRepository, AttractionRepository attractionRepository, OrderRepository orderRepository, TicketRepository ticketRepository) {
         this.maintenanceRepository = maintenanceRepository;
         this.attractionRepository = attractionRepository;
+        this.orderRepository = orderRepository;
+        this.ticketRepository = ticketRepository;
     }
 
     @Scheduled(cron = "0 1 0 * * *")
@@ -50,6 +58,27 @@ public class CleaningService {
                                 + " of attraction " + attraction.getId().toString());
                     }
                 }
+            }
+        }
+    }
+
+    @Scheduled(cron = "0 2 0 * * *")
+    @Async
+    public void disableTickets() {
+        Logger log = Logger.getLogger(this.getClass().getName());
+        Calendar calendar = Calendar.getInstance();
+        calendar.add(Calendar.DATE, -1);
+        List<Order> orders = (List<Order>) orderRepository.findExpiredOrders(calendar.getTime());
+        log.info("Disabling expired tickets...");
+        if(orders != null) {
+            for (Order order : orders) {
+                List<Ticket> tickets = order.getTickets();
+                for (Ticket ticket : tickets) {
+                    ticket.setEnabled(false);
+                    ticketRepository.saveAndFlush(ticket);
+                }
+                order.setTickets(tickets);
+                orderRepository.saveAndFlush(order);
             }
         }
     }
